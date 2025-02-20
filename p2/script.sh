4576112e1e5362ad611f3fc4b3c67821f1ec93f0
hasabir <sabirhajar048@gmail.com>
2025-02-20 17:52:32 +0100

    add app 2 and modify script

--- p2/script.sh
+++ p2/script.sh
@@ -3,7 +3,6 @@
 apk update
 apk add curl iptables openrc git
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
 curl -sfL https://get.k3s.io/ | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -
 until [ -f /etc/rancher/k3s/k3s.yaml ]; do
@@ -12,16 +11,21 @@ until [ -f /etc/rancher/k3s/k3s.yaml ]; do
 done
 chmod 644 /etc/rancher/k3s/k3s.yaml
 
+echo "192.168.56.110 app1.com" | tee -a /etc/hosts
+echo "192.168.56.110 app2.com" | tee -a /etc/hosts
+echo "192.168.56.110 app3.com" | tee -a /etc/hosts
 
-# kubectl create namespace ingress-nginx
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
 
-# until kubectl get pods -n ingress-nginx | grep -m1 'Running'; do
-#     echo "Waiting for ingress-nginx to start..."
-#     sleep 5
-# done
 
-echo "192.168.56.110 app-one.com" | tee -a /etc/hosts
+kubectl apply -f /vagrant/app1.yaml --validate=false
+kubectl apply -f /vagrant/app2.yaml --validate=false
+kubectl apply -f /vagrant/app3.yaml --validate=false
+kubectl apply -f /vagrant/ingress.yaml --validate=false
+
+
+
 
 echo "Setup complete!"
 
+
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
